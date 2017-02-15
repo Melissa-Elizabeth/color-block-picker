c0cd89413f11c7e055dd61a16f8ba9161b08079c
Avatar <avatar@Avatars-MacBook-Pro.local>
2017-02-15 09:30:44 -0600

    new stuff

--- client.js
+++ client.js
@@ -11,25 +11,27 @@ $(document).ready(function(){
     colorBox.css('background-color', colorArray[i]);
     $('#coloredBoxes').append(colorBox);
 
-}
+  }
 
   var randomColor = colorArray[randomNumber(0, colorArray.length - 1)];
 
   console.log($(this).data().color == randomColor);
 
-$('.coloredBoxes').on('click', function(){
-if ($(this).data().color == randomColor) {
-  $('#clickResult').text('You did it! Why not you play again.');
-  randomColor = colorArray[randomNumber(0, colorArray.length - 1)];
-} else {
-  $('#clickResult').text('No way! Guess again.');
-}
-});
+  $('#currentColor').text('Click on the ' + randomColor + ' block.');
 
-function randomNumber(min, max){
-return Math.floor(Math.random() * (1 + max - min) + min);
-}
+  $('.coloredBoxes').on('click', function(){
+    if ($(this).data().color == randomColor) {
+      $('#clickResult').text('You did it! Why not play again.');
+
+      randomColor = colorArray[randomNumber(0, colorArray.length - 1)];
+      $('#currentColor').text('Click on the ' + randomColor + ' block.');
+    } else {
+      $('#clickResult').text('No way! Guess again.');
+    }
+  });
+
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (1 + max - min) + min);
+  }
 
 });
-// var correctMessage = ("You are correct!");
-// var incorrectMessage = ("You are incorrect");
